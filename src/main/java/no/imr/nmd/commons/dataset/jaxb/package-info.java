1e27c8d05009a8cb9e9362782f7170e4930fa0ae
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-23 09:47:27 +0200

    Use autogenerated package-info

--- src/main/java/no/imr/nmd/commons/dataset/jaxb/package-info.java
+++
@@ -1,6 +0,0 @@
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/nmddataset/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
-    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/nmddataset/v1", prefix = "")
-})
-package no.imr.nmd.commons.dataset.jaxb;
-
